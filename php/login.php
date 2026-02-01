76a3f39ce22152e10ac86ef4cd746c042a03b360
Michey-Mouse <michelepio44@gmail.com>
2026-02-01 12:10:20 +0100

    Update login.php

--- php/login.php
+++ php/login.php
@@ -28,12 +28,17 @@ if(!$hash){
 				echo "<p> L'utente $user non esiste. <a href=\"login.html\">Riprova</a></p>";
 			}
 			else{
-				if(password_verify($pass, $hash)){
+				if(password_verify($password, $hash)){
 					echo "<p>Login Eseguito con successo</p>";
 					//Se il login è corretto, inizializziamo la sessione
 					session_start();
+                    $sql = "SELECT username FROM Account WHERE email=$1;";
+		            $prep = pg_prepare($db, "sqluser", $sql); 
+		            $ret = pg_execute($db, "sqluser", array($email));
+                    $row = pg_fetch_assoc($ret);
+                    $user = $row['username'];
 					$_SESSION['username']=$user;
-					echo "<p><a href=\"reserved.php\">Accedi</a> al contenuto riservato solo agli utenti registrati<p>";
+					echo "<p><a href=\"forum.php\">Accedi</a> al contenuto riservato solo agli utenti registrati<p>";
 				}
 				else{
 					echo 'Username o password errati. <a href="login.html">Riprova</a>';
