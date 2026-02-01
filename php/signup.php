398db2a358a0d5e203ba7fdf79c841fa531ba1b9
Michey-Mouse <michelepio44@gmail.com>
2026-02-01 17:53:59 +0100

    php

--- php/signup.php
+++ php/signup.php
@@ -1,23 +1,28 @@
 <?php
 session_start();
-$nome = $_POST['username'];
+include "./connection.php";
+
+$nome = $_POST['nome']; // Assicurati che nel form HTML il name sia 'nome'
+$username = $_POST['username']; 
 $email = $_POST['email'];
 $password = $_POST['password'];
 
-function register($password, ){
-    require "./connection.php";
-    $hash = password_hash($password, PASSWORD_DEFAULT);
-	$sql = "INSERT INTO Account(nome, username, password) VALUES($1, $2, $3)";
-	$prep = pg_prepare($db, "insertUser", $sql); 
-	$ret = pg_execute($db, "insertUser", array($nome, $email, $hash));
-	if(!$ret) {
-		echo "ERRORE QUERY: " . pg_last_error($db);
-		return false; 
-	}
-	else{
-		return true;
-	}
+function register($n, $u, $e, $p, $db) {
+    $hash = password_hash($p, PASSWORD_DEFAULT);
+    $sql = "INSERT INTO Account(nome, username, email, password) VALUES($1, $2, $3, $4)";
+    $prep = pg_prepare($db, "insertUser", $sql); 
+    $ret = pg_execute($db, "insertUser", array($n, $u, $e, $hash));
+    
+    if(!$ret) {
+        return false; 
+    }
+    return true;
 }
 
-
+if (register($nome, $username, $email, $password, $db)) {
+    header("Location: login.html");
+    exit;
+} else {
+    echo "Errore durante la registrazione: " . pg_last_error($db);
+}
 ?>
